db517fd515ef8862005d04ff8ece311b60d2ece1
Guilherme Herzog <guilhermeherzog@gmail.com>
2017-11-08 18:07:52 -0200

    Adicionando pseudo-gerador de numeros aleatórios.

--- src/Utils.js
+++ src/Utils.js
@@ -1,10 +1,13 @@
 /* 
 	Classe responsável por forneces funções auxiliares e genéricas.
 */
+const seedrandom = require('seedrandom');
+const randomSeed = seedrandom('test');
+
 class Utils {
 	// Gera um tempo de serviço exponencial e independente de outros.
 	static getRandomExp(rate){
-		return - Math.log(1 - Math.random()) / rate;
+		return - Math.log(1 - randomSeed()) / rate;
 	}
 }
 
